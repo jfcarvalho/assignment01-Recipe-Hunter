48552ce95a72b4d3fb05c400253e99c82d55a745
Jean <jfcarvalho@dcc.ufba.br>
2016-03-08 20:50:52 -0300

    Solvedgit add .

--- recipefinder/app/controllers/recipes_controller.rb
+++ recipefinder/app/controllers/recipes_controller.rb
@@ -2,7 +2,8 @@ class RecipesController < ApplicationController
 
 	def index
 		@search = params["search"] || 'chocolate'
-		#p params["search"]
-  		@recipes = Recipe.for(@search)
+		p params["search"]
+  		@recipes = Recipe.for(@search) #problema aqui, recipes não está retornando um arraylist e sim uma string
+		#p @recipes
 	end
 end	
--- recipefinder/app/models/recipe.rb
+++ recipefinder/app/models/recipe.rb
@@ -2,16 +2,16 @@ class Recipe < ActiveRecord::Base
 	include HTTParty
 
 	
-	key_value = ENV['FOOD2FORK_KEY']
+	#key_value = ENV['FOOD2FORK_KEY']
 	hostport = ENV['FOOD2FORK_SERVER_AND_PORT'] || 'www.food2fork.com'
 	base_uri "http://#{hostport}/api"
-	default_params fields: "thumb, title, socialRank", q: "key"
+	default_params key: "159692a8057862d62fa20530b8267768", field: "image_url,title,social_rank"
 	format :json
-
-	def self.for keyword
+	
+	def self.for (keyword)
 		
-		#get(base_uri, query: {q: keyword})["elements"] #problema está aqui!
-		get("http://rubygems.org/api/v1/versions/httparty.json")
+		get("/search", query: {q: keyword}) #problema está aqui!
+		#get("http://rubygems.org/api/v1/versions/httparty.json")
 		#get("http://www.food2fork.com/api")
 	end
 end
