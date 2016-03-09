cc843273c3871962f6bc02498a396be3a9fd996e
Jean <jfcarvalho@dcc.ufba.br>
2016-03-08 21:21:45 -0300

    parametros do search correto

--- recipefinder/app/controllers/recipes_controller.rb
+++ recipefinder/app/controllers/recipes_controller.rb
@@ -1,9 +1,12 @@
 class RecipesController < ApplicationController
 
 	def index
-		@search = params["search"] || 'chocolate'
 		p params["search"]
+		if !params["search"].empty?
+			@search = params["search"] 	
+		else @search = 'chocolate'
+  		end
   		@recipes = Recipe.for(@search) #problema aqui, recipes não está retornando um arraylist e sim uma string
-		#p @recipes
+		
 	end
 end	
